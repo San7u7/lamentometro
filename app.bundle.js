ae1f569b0316b93feb2a5bf0898c0ce19359a99b
San7u7 <giovanni@appress.it>
2026-07-17 09:25:35 +0200

    Change GitHub token in app.bundle.js
    Updated GitHub token to a new value.

--- app.bundle.js
+++ app.bundle.js
@@ -5,7 +5,7 @@
   var GH_OWNER = "San7u7";
   var GH_REPO = "lamentometro";
   var GH_BRANCH = "main";
-  var GH_TOKEN = "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";
+  var GH_TOKEN = "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";
   var GH_PATH = "data/stato.json";
   var GH_API = `https://api.github.com/repos/${GH_OWNER}/${GH_REPO}/contents/${GH_PATH}`;
   var ghHeaders = { Authorization: `Bearer ${GH_TOKEN}`, Accept: "application/vnd.github+json" };
